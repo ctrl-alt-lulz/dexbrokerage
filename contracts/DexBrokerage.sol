780e80a8f19569f7f86f70b0564508ba0a91dd93
Radek Ostrowski <radek@startonchain.com>
2018-10-12 14:26:09 +1100

    added visibility

--- contracts/DexBrokerage.sol
+++ contracts/DexBrokerage.sol
@@ -5,5 +5,5 @@ pragma solidity ^0.4.24;
  * @dev Interface function called from `approveAndDeposit` depositing the tokens onto the exchange
  */
 contract DexBrokerage {
-    function receiveTokenDeposit(address token, address from, uint256 amount);
+    function receiveTokenDeposit(address token, address from, uint256 amount) public;
 }
